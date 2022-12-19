68831fb7c75952340f3a7a7280113cec3e1ed81b
Akshay M Acharya <80585215+Aks486@users.noreply.github.com>
2022-12-19 21:41:19 +0530

    Add files via upload

---
+++ Q1/Code/main.c
@@ -0,0 +1,14 @@
+#include"led.h"
+
+int main()
+{ 
+	PINSEL0|=0xF0F0;
+	IODIR0|=(0XF<<16); 
+	VICIntEnable = (3<<14);
+	VICVectAddr0 = (unsigned int)upcount; // Address of the ISR
+  VICVectCntl0 = (1<<5)| 14; 
+	VICVectAddr1 = (unsigned int)downcount; // Address of the ISR
+  VICVectCntl1 = (1<<5)| 15;   
+	while(1);
+	return 0;
+}
